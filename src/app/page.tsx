5f293dad383260ee3a59f029fec6911e9712d697
susanta96 <svchost96@gmail.com>
2023-10-05 11:35:38 +0530

    chore: updated latest changes to welcome website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex flex-col lg:flex-row p-3 w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p className="flex flex-col lg:flex-row p-3 w-full justify-center  border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Want to become part of the community?&nbsp;
           <code className="font-mono font-bold">https://github.com/AS-Devs</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
       {/* <div className='w-full'> */}
-      <h1 className='font-mono text-center text-2xl pt-4 md:text-4xl'>Welcome to AS Developers !!</h1>
+      <img src="https://ik.imagekit.io/4gk79wff3ko/blogs/banner_s9OdsJMBC.png?tr=h-450,f-auto" alt="banner as devs" width="auto" height="auto" />
+      {/* <h1 className='font-mono text-center text-2xl pt-4 md:text-4xl'>Welcome to AS Developers !!</h1> */}
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://api.asdevs.dev"
@@ -48,36 +31,36 @@ export default function Home() {
         </a>
 
         <a
-          href="https://gist.asdevs.dev"
+          href="https://blog.asdevs.dev"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Gists{' '}
+            Blogs &nbsp;
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-           Save & Share the Gist with Snibox
+           We've started writing technical blogs which will help other developers, as well as help power user. 
           </p>
         </a>
 
         <a
-          href="https://s.asdevs.dev"
+          href="https://uploader.asdevs.dev"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Susanta Website{' '}
+            Image Uploader &nbsp;
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Susanta&apos;s Portfolio
+            Our own image / file uplader which will help you to upload different files , and generate long lived URL with automatic optimizations.
           </p>
         </a>
 
